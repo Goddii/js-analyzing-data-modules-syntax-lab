8d341061e46cdf773f09a85a177e2fd364934982
Goddii <otienogodwin23@gmail.com>
2026-04-16 12:56:11 +0300

    add combineuser function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
+const combineUsers = (...arrays) => {
+  const users = arrays.flat()
 
+  const merge_date = Date.today().toString("M/d/yyyy")
+
+  return {users, merge_date}
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
